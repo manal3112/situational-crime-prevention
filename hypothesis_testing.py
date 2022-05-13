2c853722444058d308fcc630f099ac384d8b3039
Dhruv Verma <dhruv2scs@gmail.com>
2022-05-12 23:00:09 -0400

    Added support to test multiple features

--- hypothesis_testing.py
+++ hypothesis_testing.py
@@ -10,51 +10,38 @@ Original file is located at
 import pyspark
 import sys
 import csv
-import ast
+import argparse
 import numpy as np
 import math
 from scipy.stats import t
-
-COUNTIES_DATA_PATH = "/content/drive/MyDrive/BDA/CSE_545_Project/Other_data/all_counties_df.csv"
-UNEMPLOYMENT_DATA_PATH = "/content/drive/MyDrive/BDA/CSE_545_Project/Other_data/Unemployment.csv"
-
-sc = pyspark.SparkContext()
-
-counties_data = sc.textFile(COUNTIES_DATA_PATH)
-unemployment_data = sc.textFile(UNEMPLOYMENT_DATA_PATH)
-counties_data = counties_data.mapPartitions(lambda line: csv.reader(line))
-unemployment_data = unemployment_data.mapPartitions(lambda line: csv.reader(line))
-
-counties_data_header = counties_data.first()
-unemployment_data_header = unemployment_data.first()
-
-counties_data_header_dict = {k.strip(): v for v, k in enumerate(counties_data_header)}
-unemployment_data_header_dict = {k.strip(): v for v, k in enumerate(unemployment_data_header)}
+import pprint
 
 def create_county_tuples(tup):
-  fips_code = tup[counties_data_header_dict['FIPS_Code']]
-  year = tup[counties_data_header_dict['Year']]
-  for key, value in counties_data_header_dict.items():
+  crime_data_header_dict = crime_dict.value
+  fips_code = tup[crime_data_header_dict['FIPS_Code']]
+  year = tup[crime_data_header_dict['Year']]
+  for key, value in crime_data_header_dict.items():
     if key not in ['FIPS_Code', 'Year']:
       yield ((int(fips_code), int(year)), (key, float(tup[value])))
 
-def create_unemployment_tuples(tup):
-  fips_code = tup[unemployment_data_header_dict['FIPS_Code']]
-  year_names = ['2001', '2002', '2003', '2004', '2005']
-  for year_name in year_names:
-    col_name = 'Unemployment_rate_' + year_name
-    rate = tup[unemployment_data_header_dict[col_name]].strip()
+def create_feature_tuples(tup):
+  feature_data_header_dict = feature_dict.value
+  fips_code = tup[feature_data_header_dict['FIPS_Code']]
+  years = years_list.value
+  for year in years:
+    col_name = feature_name.value + "_" + year
+    rate = tup[feature_data_header_dict[col_name]].strip()
     if len(rate) > 0:
-      yield ((int(fips_code), int(year_name[-1])), float(rate))
+      yield ((int(fips_code), int(year[-1])), float(rate))
 
 def get_correlation_val(data):
   offence_code = data[0]
   arrest_rate = []
-  unemployment_rate = []
+  feature_rate = []
   for arrest_val, unemploy_val in data[1]:
     arrest_rate.append(arrest_val)
-    unemployment_rate.append(unemploy_val)
-  corr = np.corrcoef(arrest_rate, unemployment_rate)
+    feature_rate.append(unemploy_val)
+  corr = np.corrcoef(arrest_rate, feature_rate)
   return (offence_code, (corr[0][1], len(arrest_rate) - 2))
 
 def get_p_values(tup):
@@ -66,13 +53,52 @@ def get_p_values(tup):
   corrected_p_value = p_value * correction_val
   return (offence_type, (pearson_corr, p_value, corrected_p_value))
 
-counties_temp = counties_data.filter(lambda row: row != counties_data_header).flatMap(lambda t: create_county_tuples(t))
-unemployment_temp = unemployment_data.filter(lambda row: row != unemployment_data_header).flatMap(lambda t: create_unemployment_tuples(t))
+def parse_args():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("crime_data_path", type=str)
+  parser.add_argument("feature_data_path", type=str)
+  parser.add_argument("feature_name", type=str)
+  parser.add_argument('--years_list', nargs='+', default=['2001', '2002', '2003', '2004', '2005'])
+  return parser.parse_args()
 
-joined_data = unemployment_temp.join(counties_temp)
-grouped_joined_data = joined_data.groupByKey().mapValues(list)
-flattened_grouped_joined_data = grouped_joined_data.flatMap(lambda tup: tup[1])
-flattened_grouped_joined_data = flattened_grouped_joined_data.map(lambda tup: (tup[1][0], (tup[1][1], tup[0])))
-grouped_by_offence_type = flattened_grouped_joined_data.groupByKey().mapValues(list)
-corr_list = grouped_by_offence_type.map(lambda tup: get_correlation_val(tup)).map(lambda tup: get_p_values(tup)).collect()
-print(sorted(corr_list, key=lambda tup: tup[1][0], reverse=True))
+if __name__ == "__main__":
+  args = parse_args()
+  print(args)
+
+  crime_data_path = args.crime_data_path
+  feature_data_path = args.feature_data_path
+  feature_name = args.feature_name
+  years_list = args.years_list
+
+  sc = pyspark.SparkContext()
+
+  crime_data = sc.textFile(crime_data_path)
+  feature_data = sc.textFile(feature_data_path)
+  crime_data = crime_data.mapPartitions(lambda line: csv.reader(line))
+  feature_data = feature_data.mapPartitions(lambda line: csv.reader(line))
+
+  crime_data_header = crime_data.first()
+  feature_data_header = feature_data.first()
+
+  crime_data_header_dict = {k.strip(): v for v, k in enumerate(crime_data_header)}
+  feature_data_header_dict = {k.strip(): v for v, k in enumerate(feature_data_header)}
+
+  crime_dict = sc.broadcast(crime_data_header_dict)
+  feature_dict = sc.broadcast(feature_data_header_dict)
+  years_list = sc.broadcast(years_list)
+  feature_name = sc.broadcast(feature_name)
+
+  crime_temp = crime_data.filter(lambda row: row != crime_data_header).flatMap(lambda t: create_county_tuples(t))
+  feature_temp = feature_data.filter(lambda row: row != feature_data_header).flatMap(lambda t: create_feature_tuples(t))
+
+  joined_data = feature_temp.join(crime_temp)
+  grouped_joined_data = joined_data.groupByKey().mapValues(list)
+  flattened_grouped_joined_data = grouped_joined_data.flatMap(lambda tup: tup[1])
+  flattened_grouped_joined_data = flattened_grouped_joined_data.map(lambda tup: (tup[1][0], (tup[1][1], tup[0])))
+  grouped_by_offence_type = flattened_grouped_joined_data.groupByKey().mapValues(list)
+  corr_list = grouped_by_offence_type.map(lambda tup: get_correlation_val(tup)).map(lambda tup: get_p_values(tup)).collect()
+  corr_list = sorted(corr_list, key=lambda tup: tup[1][0], reverse=True)
+  file_name = feature_name.value + "_corr_" + years_list.value[0] + "-" + years_list.value[-1] + ".txt"
+  output_file = open(file_name, 'w')
+  pp = pprint.PrettyPrinter(indent=2, compact=True, stream=output_file)
+  pp.pprint(corr_list)
